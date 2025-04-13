b5db67be5dc4abb5bb8038a11bd5c738780513fe
nyy0606 <50909870+nyy0606@users.noreply.github.com>
2025-04-13 20:33:53 +0900

    String

---
+++ CSharp 1/CSharp 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+using System.Threading.Channels;
+
+namespace CSharp_1
+{  
+    class Program
+	{
+        static void Main(string[] args)
+        {
+            string name = "Harry Potter";
+
+            bool found = name.Contains("Harry"); //문자열이 이 안에 있는지
+
+            int index = name.IndexOf('P'); // 특정 문자가 이 안에 몇번째 인덱스에 있는지/ 없으면 -1
+
+
+            name = name + " Junior";
+            Console.WriteLine(name);
+
+            string lowcasename = name.ToLower();
+            string uppercasename = name.ToUpper();
+
+            string newname = name.Replace('r', 'l'); //r을 l로 변경
+
+
+            string[] names = name.Split(new char[] { ' ' }); //띄어쓰기를 기준으로 나눠서 저장
+
+        }
+    }
+}
+  
